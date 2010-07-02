e0bd24ccc82169d05acffa7c0be58e76cff01a3a
F. Markham <fmarkham@gmail.com>
2010-07-02 19:58:12 +1000

    small changes

--- shpshift/shp.py
+++ shpshift/shp.py
@@ -15,19 +15,31 @@ from util import ColumnType
 from util import ColumnGeometry
 from util import InvalidFilenameError
 
+ogr.UseExceptions()
+
 class ShpWriter(object): 
     def __init__(self, filename, fields, srs=None, overwrite=False):
         self._filename = filename
         self._fields = fields
         self._srs = srs
         self._overwrite = overwrite
+        self._validate_filename()
         if overwrite:
             self._delete_any_shapefiles()
         shps = list(self._find_shapefiles())
         if len(shps) > 0:
             raise IOError, "Files %s and '%s' already exist.  Use '--force' to overwrite." % (
                 ', '.join(["'%s'" % s for s in shps[:-1]]), shps[-1])
-            
+        self._create_shp()
+        
+        
+    def _create_shp(self):
+        drv = ogr.GetDriverByName( 'ESRI Shapefile' )
+        try:
+            self._ds = drv.CreateDataSource(self._filename)
+            self._lyr = self._ds.CreateLayer( "points", self._srs, ogr.wkbPoint )
+        except Exception, e:
+            raise IOError, "Could not create shapefile '%s': %s" % (self._filename, e)
 
     def _find_shapefiles(self):
         shp_exts = ['.shp', '.shx', '.dbf', '.prj', '.sbn', '.sbx', '.qix']
@@ -41,6 +53,11 @@ class ShpWriter(object):
             _, ext = os.path.splitext(f)
             if ext in shp_exts:
                 yield f
+    
+    def _validate_filename(self):
+        _, ext = os.path.splitext(self._filename)
+        if ext != '.shp':
+            raise InvalidFilenameError, "'%s' is not a valid shapefile filename" % self._filename
 
     def _delete_any_shapefiles(self):
         for f in self._find_shapefiles():
